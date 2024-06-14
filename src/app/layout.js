2187f3dea46d955e80fd1c3353ff94031f599eed
MuhammadHassan125 <hassanalirajput2004@gmail.com>
2024-06-14 12:06:24 +0500

    conical-image

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,11 +11,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="scroll-smooth" 
-    style={{overflowX: 'hidden'}}
-    >
+    <html lang="en" className="scroll-smooth" style={{ overflowX: "hidden" }}>
       <head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
@@ -32,12 +30,20 @@ export default function RootLayout({ children }) {
           type="text/css"
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
         />
+        <meta property="og:image" content="/og-image.jpg" />
+        <meta property="og:title" content="Sales-Focused Marketing" />
+        <meta
+          property="og:description"
+          content="Our Objective: Increase Your Revenue"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="" />{" "}
       </head>
 
       <body className={inter.className}>
-          <CustomCursor/>
+        <CustomCursor />
         {children}
-        </body>
+      </body>
     </html>
   );
 }
